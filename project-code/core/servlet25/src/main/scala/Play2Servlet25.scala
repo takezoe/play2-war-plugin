bb377c81cb6690398566c44b30e6650de4834da2
takezoe <takezoe@gmail.com>
2013-04-09 12:00:42 +0900

    Fixed indent.

--- project-code/core/servlet25/src/main/scala/Play2Servlet25.scala
+++ project-code/core/servlet25/src/main/scala/Play2Servlet25.scala
@@ -19,7 +19,7 @@ object Play2Servlet {
 class Play2Servlet extends GenericPlay2Servlet[Tuple3[HttpServletRequest, HttpServletResponse, Object]] with Helpers {
 
   protected override def onBeginService(request: HttpServletRequest, response: HttpServletResponse): Tuple3[HttpServletRequest, HttpServletResponse, Object] = {
-     (request, response, new AtomicBoolean(false))
+    (request, response, new AtomicBoolean(false))
   }
 
   protected override def onFinishService(execContext: Tuple3[HttpServletRequest, HttpServletResponse, Object]) = {
@@ -29,14 +29,14 @@ class Play2Servlet extends GenericPlay2Servlet[Tuple3[HttpServletRequest, HttpSe
       }
     }
   }
-  
+
   protected override def onHttpResponseComplete(execContext: Tuple3[HttpServletRequest, HttpServletResponse, Object]) = {
     execContext._3.synchronized {
       execContext._3.asInstanceOf[AtomicBoolean].set(true)
       execContext._3.notify()
     }
   }
-  
+
   protected override def getHttpRequest(executionContext: Tuple3[HttpServletRequest, HttpServletResponse, Object]): RichHttpServletRequest = {
     new RichHttpServletRequest {
       def getRichInputStream(): Option[java.io.InputStream] = {
@@ -44,16 +44,16 @@ class Play2Servlet extends GenericPlay2Servlet[Tuple3[HttpServletRequest, HttpSe
       }
     }
   }
-  
+
   protected override def getHttpResponse(executionContext: Tuple3[HttpServletRequest, HttpServletResponse, Object]): RichHttpServletResponse = {
     new RichHttpServletResponse {
-        def getRichOutputStream: Option[java.io.OutputStream] = {
-          Option(executionContext._2.getOutputStream)
-        }
+      def getRichOutputStream: Option[java.io.OutputStream] = {
+        Option(executionContext._2.getOutputStream)
+      }
   
-        def getHttpServletResponse: Option[HttpServletResponse] = {
-          Option(executionContext._2)
-        }
+      def getHttpServletResponse: Option[HttpServletResponse] = {
+        Option(executionContext._2)
+      }
     }
   }
 
